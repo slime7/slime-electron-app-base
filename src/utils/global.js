69bbff67994d37ec3023ffae78bc4c37bb2b2c15
Slime7 <slime7@foxmail.com>
2019-09-18 13:36:09 +0800

    fix app name

--- src/utils/global.js
+++ src/utils/global.js
@@ -3,7 +3,7 @@ import path from 'path';
 import Store from 'electron-store';
 
 global.ROOT = path.join(__dirname, '..');
-global.APPDATA_PATH = path.join(app.getPath('appData'), 'wow-manager');
+global.APPDATA_PATH = path.join(app.getPath('appData'), 'slime-electron-app-base');
 global.win = null;
 global.store = new Store({
   serialize: value => JSON.stringify(value, null, '  '),
